d6f5829a3b9f6c1e7f9930441e168b3f31a221cf
Tyler <tylerdh12@gmail.com>
2019-05-28 09:45:51 -0700

    Final push for final check
    Still may work on the extra credit portion but project completed to worksheet directions

--- js/script.js
+++ js/script.js
@@ -3,25 +3,25 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 Tyler Harper
 ******************************************/
+// pull the content and set as variables
+let pageList = document.querySelectorAll('ul');
+let list = document.querySelectorAll('li');
 
-const pageList = document.querySelectorAll('ul');
-const list = document.querySelectorAll('li');
-
-//set number of students to display per page
+//use this to set the number of students to display per page
 const studentsPerPage = 10;
 
-//setting the default page for page load
+//setting the default page for initial page load
 const page = 1;
 
-//the math to figure out how many pages we are going to have
-const numberOfPages = (Math.ceil(list.length/studentsPerPage));
+//the math to figure out how many pages we are going to have as links
+let numberOfPages = (Math.ceil(list.length/studentsPerPage));
 
 
-//Function to show only 10 students 
+//Function to show only 10 students
 function showPage(list, page) {
    //assign index of students
    for (let i = 0; i < list.length; i++) {
-      //show 10 students (using -1 for correction) using index 
+      //show 10 students (using -1 for correction) using index
       if (i >= (page -1)*studentsPerPage && i < page * studentsPerPage) {
          list[i].style.display = '';
       } else {
@@ -30,9 +30,9 @@ function showPage(list, page) {
       }
    }
 }
-
-function appendPageLinks(){   
-   //need to create the container for our pagination lines--
+// function to display the links at the bottom of page
+function appendPageLinks(){
+   //need to create the container for our pagination links--
    const pagination = document.createElement('div');
    const pageDiv = document.querySelector('body div');
       pagination.className = "pagination";
@@ -49,71 +49,71 @@ function appendPageLinks(){
 
          paginationList.appendChild(pageElement);
          pageElement.appendChild(pageLink);
-         
+
          //add the numbering system for each button
          pageLink.textContent = i+1;
 
          pageLink.classList.remove('active');
 
-         // listening for a click on the buttons to rerun functions with the output of the page and list 
+         // listening for a click on the buttons to rerun functions with the output of the page and list
          pageLink.addEventListener("click", () => {
             showPage(list, i+1);
             pageLink.classList.remove('active');
          });
    }
-}   
-
-//needs to run to hide the excess on initial load
+}
+//fist run of show page to show the beginning of the list on page 1
 showPage(list, page);
-
-
+//shows the page links
 appendPageLinks();
 
+//Extra Credit:
 
-function appendSearch() {
-   const searchDiv = document.createElement('div');
-   const searchAppend = searchDiv.append;
-   searchDiv.className = "student-search";
+// function appendSearch() {
+//    const searchDiv = document.createElement('div');
+//    const searchAppend = searchDiv.append;
+//    searchDiv.className = "student-search";
+//
+//    const searchInput = document.createElement("input");
+//    searchInput.type = "text";
+//    searchInput.placeholder = "Search for students...";
+//
+//    const searchBtn = document.createElement("button");
+//    searchBtn.type = "submit";
+//    searchBtn.textContent = "Search";
+//
+//    document.querySelector('.page-header').appendChild(searchDiv);
+//    searchDiv.appendChild(searchInput);
+//    searchDiv.appendChild(searchBtn);
+//
+// }
 
-   const searchInput = document.createElement("input");
-   searchInput.type = "text";
-   searchInput.placeholder = "Search for students...";
-
-   const searchBtn = document.createElement("button");
-   searchBtn.type = "submit";
-   searchBtn.textContent = "Search";
-
-   document.querySelector('.page-header').appendChild(searchDiv);
-   searchDiv.appendChild(searchInput);
-   searchDiv.appendChild(searchBtn);
-
-}
-
-appendSearch();
-
-
-// Function should check the text content of input with the child elements of the list to see if anything matches if so appear in the list if not hide if nothing show message saying nothing was found please check your input
-   const searchContent = document.querySelector('input');
-   const searchBtn = document.querySelector('button');
-   const namesList = document.querySelectorAll('h3');
-   const searchResults = [];
-
-searchContent.addEventListener('keyup', () => {
-
-   let filter = searchContent.value.toLowerCase();
-
-   for (let i = 0; i < namesList.length; i++){
-
-      if (list[i].innerHTML.indexOf(filter) > -1) {
-         list[i].style.display = '';
-      } else {
-         list[i].style.display = 'none';
-         searchResults.push(i);
-      }
-
-   }
-
-
-});
+// appendSearch();
+//
+//
+// // Function should check the text content of input with the child elements of the list to see if anything matches if so appear in the list if not hide if nothing show message saying nothing was found please check your input
+//    const searchContent = document.querySelector('input');
+//    const searchBtn = document.querySelector('button');
+//    const namesList = document.querySelectorAll('h3');
+//    const searchResults = [];
+//
+// searchContent.addEventListener('keyup', () => {
+//
+//    let filter = searchContent.value.toLowerCase();
+//
+//    for (let i = 0; i < namesList.length; i++){
+//      list[i].style.display = '';
+//
+//       if (list[i].innerHTML.indexOf(filter) > -1) {
+//       } else {
+//          list[i].style.display = 'none';
+//          searchResults.push(list);
+//       }
+//
+//    }
+//
+//    showPage(searchResults, 1);
+//    appendPageLinks(searchResults);
+// });
 
 //showPage(list, page);
